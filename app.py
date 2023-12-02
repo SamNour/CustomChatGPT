2f97523c3abda6768e5d67bb7353f210e15cf1d4
super mario <mario160@mail.ru>
2023-12-03 01:45:55 +0200

    Working api conversation

--- app.py
+++ app.py
@@ -3,24 +3,19 @@ import os
 import streamlit as st
 from streamlit_chat import message
 import os, requests
+from promptflow import tool
+import json
 
-'''
-    Place for the AI keys and stuff
-'''
+# Place for the AI keys and stuff
 openai.api_type = "azure"
-# Azure OpenAI on your own data is only supported by the 2023-08-01-preview API version
 openai.api_version = "2023-08-01-preview"
+openai.api_base = "https://openai-bottum-france.openai.azure.com/"
+openai.api_key = '13f668bdda354088ba9c441486d90c57'
+deployment_id = "gpt-4-tt"
 
-# Azure OpenAI setup
-openai.api_base = "https://openai-bottum-france.openai.azure.com/" # Add your endpoint here
-openai.api_key = os.getenv("OPENAI_API_KEY") # Add your OpenAI API key here
-deployment_id = "gpt-4-tt" # Add your deployment ID here
-
-# Azure AI Search setup
-search_endpoint = "https://azureaisearch-bottum.search.windows.net"; # Add your Azure AI Search endpoint here
-search_key = os.getenv("SEARCH_KEY"); # Add your Azure AI Search admin key here
-search_index_name = "michelle2"; # Add your Azure AI Search index name here
-
+search_endpoint = "https://azureaisearch-bottum.search.windows.net"
+search_key = 'NElngw2d8eVnBatvpiJlfcAT3l6FrfmFQGINybEenIAzSeD4xiue'
+search_index_name = "index-tt11"
 
 def setup_byod(deployment_id: str) -> None:
     """Sets up the OpenAI Python SDK to use your own data for the chat endpoint.
@@ -48,10 +43,7 @@ def setup_byod(deployment_id: str) -> None:
 
 setup_byod(deployment_id)
 
-'''
-    Place for UI
-'''
-
+# Place for UI
 st.title("BotTUM")
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -84,12 +76,18 @@ if prompt := st.chat_input("What is up?"):
                         "endpoint": search_endpoint,
                         "key": search_key,
                         "indexName": search_index_name,
+                        "embeddingEndpoint": f"{openai.api_base}/openai/deployments/ada-tt/embeddings?api-version={openai.api_version}",
+                        "embeddingKey": openai.api_key,
+                        "queryType": "vectorSimpleHybrid",
+                        "roleInformation": "Your name is botTUM. You are a helpful and kind assistant for students at the Technical University of Munich (Technische Universität München). Your job is to answer students’ queries about their studies. Be brief in your answers. You should answer only with the facts listed in the list of sources below. If there is not enough information below, say you don’t know. Do not generate answers that don’t use the sources below. If asking a clarifying question to the user would help, ask the question. Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brackets to reference the source, e.g. [info1.txt]. Don’t combine sources, list each source separately, e.g. [info1.txt][info2.pdf][info3.pptx][info4.docx]."
                     }
                 }
             ],
+            temperature=.4,
             stream=True
         ):
-            full_response += (response.choices[0].delta.content or "")
+            print(response)
+            full_response += (response["choices"][0]["delta"].get("content", None) or "")
             message_placeholder.markdown(full_response + "▌")
     else:
         message_placeholder.markdown(full_response)
--- test.py
+++ test.py
@@ -1,18 +1,14 @@
 import openai, os, requests
 
 openai.api_type = "azure"
-# Azure OpenAI on your own data is only supported by the 2023-08-01-preview API version
-# openai.api_version = "2023-10-01-preview"
+openai.api_version = "2023-08-01-preview"
+openai.api_base = "https://openai-bottum-france.openai.azure.com/"
+openai.api_key = '13f668bdda354088ba9c441486d90c57'
+deployment_id = "gpt-4-tt"
 
-# Azure OpenAI setup
-api_base = "https://openai-bottum-france.openai.azure.com/" # Add your endpoint here
-# openai.api_key = os.getenv("OPENAI_API_KEY") # Add your OpenAI API key here
-deployment_id = "gpt-4-tt" # Add your deployment ID here
-
-# Azure AI Search setup
-search_endpoint = "https://azureaisearch-bottum.search.windows.net"; # Add your Azure AI Search endpoint here
-search_key = os.getenv("SEARCH_KEY"); # Add your Azure AI Search admin key here
-search_index_name = "michelle2"; # Add your Azure AI Search index name here
+search_endpoint = "https://azureaisearch-bottum.search.windows.net"
+search_key = 'NElngw2d8eVnBatvpiJlfcAT3l6FrfmFQGINybEenIAzSeD4xiue'
+search_index_name = "index-tt11"
 
 def setup_byod(deployment_id: str) -> None:
     """Sets up the OpenAI Python SDK to use your own data for the chat endpoint.
@@ -25,14 +21,14 @@ def setup_byod(deployment_id: str) -> None:
     class BringYourOwnDataAdapter(requests.adapters.HTTPAdapter):
 
         def send(self, request, **kwargs):
-            request.url = f"{api_base}/openai/deployments/{deployment_id}/extensions/chat/completions?api-version={openai.api_version}"
+            request.url = f"{openai.api_base}/openai/deployments/{deployment_id}/extensions/chat/completions?api-version={openai.api_version}"
             return super().send(request, **kwargs)
 
     session = requests.Session()
 
     # Mount a custom adapter which will use the extensions endpoint for any call using the given `deployment_id`
     session.mount(
-        prefix=f"{api_base}/openai/deployments/{deployment_id}",
+        prefix=f"{openai.api_base}/openai/deployments/{deployment_id}",
         adapter=BringYourOwnDataAdapter()
     )
 
@@ -42,26 +38,23 @@ setup_byod(deployment_id)
 
 
 message_text = [{"role": "user", "content": "What are the differences between Azure Machine Learning and Azure AI services?"}]
-
-client = openai.AzureOpenAI(
-    api_key = os.getenv("OPENAI_API_KEY"),
-    azure_endpoint=api_base,
-    api_version="2023-07-01-preview"
-)
-
-completion = client.chat.completions.create(
-    messages=message_text,
-    model=deployment_id,
-    extra_body={"dataSources": [  # camelCase is intentional, as this is the format the API expects
-        {
-            "type": "AzureCognitiveSearch",
-            "parameters": {
-                "endpoint": search_endpoint,
-                "key": search_key,
-                "indexName": search_index_name,
-                # "accessToken": "c8yQH07JcPuFIhVg38bdbKGTTqjUGMw1f8EnLgfhVAAzSeCZqqcK"
+# message_text = [{"role": "user", "content": message_text}]
+completion = openai.ChatCompletion.create(
+        messages=message_text,
+        deployment_id=deployment_id,
+        dataSources=[  # camelCase is intentional, as this is the format the API expects
+            {
+                "type": "AzureCognitiveSearch",
+                "parameters": {
+                    "endpoint": search_endpoint,
+                    "key": search_key,
+                    "indexName": search_index_name,
+                    "embeddingEndpoint": f"{openai.api_base}/openai/deployments/ada-tt/embeddings?api-version={openai.api_version}",
+                    "embeddingKey": openai.api_key,
+                    "queryType": "vectorSimpleHybrid",
+                    "roleInformation": "Your name is botTUM. You are a helpful and kind assistant for students at the Technical University of Munich (Technische Universität München). Your job is to answer students’ queries about their studies. Be brief in your answers. You should answer only with the facts listed in the list of sources below. If there is not enough information below, say you don’t know. Do not generate answers that don’t use the sources below. If asking a clarifying question to the user would help, ask the question. Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brackets to reference the source, e.g. [info1.txt]. Don’t combine sources, list each source separately, e.g. [info1.txt][info2.pdf][info3.pptx][info4.docx]."
+                }
             }
-        }
-    ]}
-)
+        ]
+    )
 print(completion)
